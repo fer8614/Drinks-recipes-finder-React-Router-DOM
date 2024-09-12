07440c991a83994f7b31ea63b70e54a8ecc153d1
Fernando <fernando.inversionmf@gmail.com>
2024-09-11 21:45:27 -0700

    Creating the Slice for favorites and adding favorites

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,7 +1,10 @@
 import { create } from 'zustand'
 import { devtools } from 'zustand/middleware'
 import { createRecipeSlice, recipesSliceType } from '../stores/recipeSlice'
+import { createFavoritesSlice, FavoritesSliceType } from '../stores/favoritesSlice'
 
-export const useAppStore = create<recipesSliceType>()( devtools( ( ...a ) => ({ 
+export const useAppStore = create<recipesSliceType & FavoritesSliceType>()( devtools( ( ...a ) => ({ 
     ...createRecipeSlice( ...a ),
+    ...createFavoritesSlice( ...a ),
+
 })))
