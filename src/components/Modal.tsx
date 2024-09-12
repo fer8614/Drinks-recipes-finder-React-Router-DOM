19f19f51c6b256a92d5204a3b19eab61f8eead6b
Fernando <fernando.inversionmf@gmail.com>
2024-09-11 18:34:41 -0700

    Creating the close and add to favorites button in the modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -71,6 +71,23 @@ export default function Modal() {
                   </Dialog.Title>
 
                   <p className='text-lg'>{ selectedRecipe.strInstructions }</p>
+
+                  <div className='mt-5 flex justify-between gap-4'>
+                    <button
+                      type='button'
+                      className='w-full rounded bg-gray-600 p-3 font-bold uppercase text-white shadow hover    :bg-gray-500' 
+                      onClick={closeModal} 
+                    >
+                      Close
+                    </button>
+
+                    <button
+                      type='button'
+                      className='w-full rounded bg-orange-600 p-3 font-bold uppercase text-white shadow hover    :bg-gray-500'  
+                    >
+                      Add to favorites
+                    </button>
+                  </div>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
